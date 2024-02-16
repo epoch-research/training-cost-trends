5e82bac3c32bb7d16b7b5ddef8d90f8967021097
Ben Cottier <bcottier2@gmail.com>
2024-02-16 18:38:10 +0000

    Add most_common_impute_training_hardware

--- imputation.py
+++ imputation.py
@@ -4,6 +4,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.impute import KNNImputer
 from sklearn.preprocessing import LabelEncoder
 
+from data import *
 from utils import *
 
 
@@ -190,6 +191,30 @@ def most_common_impute(dataframe, target_col, time_col):
     return dataframe
 
 
+def most_common_impute_training_hardware(pcd_df):
+    """
+    Impute the missing values in the `Training hardware` of `pcd_df` with the most common value
+    for each year in the full PCD data.
+    """
+    # Load full PCD data to get as much data as possible
+    full_pcd_df = load_pcd_df()
+
+    # Publication date in datetime format
+    full_pcd_df.dropna(subset=['Publication date'], inplace=True)
+    full_pcd_df['Publication date'] = pd.to_datetime(full_pcd_df['Publication date'])
+    full_pcd_df['Publication date'] = datetime_to_float_year(full_pcd_df['Publication date'])
+
+    # Impute missing values in Training hardware
+    imputed_pcd_df = most_common_impute(full_pcd_df, 'Training hardware', 'Publication date')
+
+    frontier_systems = load_frontier_systems()
+    pcd_df.loc[:, 'Training hardware'] = imputed_pcd_df.loc[
+        imputed_pcd_df['System'].isin(frontier_systems), 'Training hardware'
+    ]
+    
+    return pcd_df
+
+
 def drop_random_values(target_df, target_col, reference_df, reference_col, num_drop):
     """
     Set `num_drop` random values in `target_col` to NaN in `target_df`.
@@ -204,22 +229,3 @@ def drop_random_values(target_df, target_col, reference_df, reference_col, num_d
     dropped_df.loc[holdout_values.index, target_col] = np.nan
     return dropped_df, holdout_values
 
-
-if __name__ == '__main__':
-    from cost import *
-
-    # Load the data
-    pcd_df = pd.read_csv('data/All ML Systems - full view.csv')
-
-    # Publication date in datetime format
-    pcd_df.dropna(subset=['Publication date'], inplace=True)
-    pcd_df['Publication date'] = pd.to_datetime(pcd_df['Publication date'])
-    pcd_df['Publication date'] = datetime_to_float_year(pcd_df['Publication date'])
-
-    # Impute missing values in Training hardware
-    imputed_pcd_df = most_common_impute(pcd_df, 'Training hardware', 'Publication date')
-
-    frontier_pcd_df, hardware_df, price_df = load_data_for_cost_estimation()
-    print(frontier_pcd_df['Training hardware'])
-    frontier_pcd_df.loc[:, 'Training hardware'] = imputed_pcd_df.loc[imputed_pcd_df['System'].isin(frontier_systems), 'Training hardware']
-    print(frontier_pcd_df['Training hardware'])
