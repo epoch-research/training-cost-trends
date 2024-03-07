4766c659bc52a2ae5d25ae1736c6bb1f21565e77
Ben Cottier <bcottier2@gmail.com>
2024-03-07 14:01:49 -0500

    Add growth rate conversion utilities

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 
@@ -12,3 +13,29 @@ def float_year_to_datetime(float_year):
     days_in_year = 365 + int(pd.Timestamp(year=year, month=12, day=31).is_leap_year)
     day_of_year = int(remainder * days_in_year)
     return pd.Timestamp(year=year, month=1, day=1) + pd.to_timedelta(day_of_year, unit='D')
+
+
+def ooms_to_factor_per_year(ooms):
+    return 10 ** ooms
+
+
+def factor_per_year_to_ooms(factor_per_year):
+    return np.log10(factor_per_year)
+
+
+def ooms_to_doublings_per_year(ooms):
+    return ooms * np.log2(10)
+
+
+def doublings_per_year_to_ooms(doublings_per_year):
+    return doublings_per_year / np.log2(10)
+
+
+def ooms_to_doubling_time_months(ooms):
+    doubling_time_years = 1 / ooms_to_doublings_per_year(ooms)
+    return doubling_time_years * 12
+
+
+def doubling_time_months_to_ooms(doubling_time_months):
+    doubling_time_years = doubling_time_months / 12
+    return doublings_per_year_to_ooms(1 / doubling_time_years)
