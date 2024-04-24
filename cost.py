1f2061b76550f66ae6529d440c5032790a478c8b
Ben Cottier <bcottier2@gmail.com>
2024-04-24 15:37:39 -0400

    CapEx + OpEx model

--- cost.py
+++ cost.py
@@ -244,3 +244,131 @@ def estimate_upfront_server_capex(
     frontier_pcd_df['Cost'] = frontier_pcd_df['System'].map(system_to_cost)
 
     return frontier_pcd_df
+
+
+def estimate_hardware_capex_opex(
+    frontier_pcd_df,
+    hardware_df,
+    price_df,
+    impute_pcd_fn=None,
+    **impute_kwargs,
+):
+    """
+    Full pipeline for estimating the sum of amortized hardware CapEx and OpEx
+    """
+    if impute_pcd_fn is not None:
+        frontier_pcd_df = impute_pcd_fn(frontier_pcd_df, **impute_kwargs)
+    
+    pcd_hardware_model_colname = 'Training hardware'
+    price_colname = 'Price (hardware purchase)'
+    system_to_price = {}
+
+    for i, row in frontier_pcd_df.iterrows():
+        # TODO
+        price, _ = find_purchase_price(
+            row, price_df, hardware_df, pcd_hardware_model_colname, price_colname
+        )
+        if price is None:
+            continue
+        else:
+            system_to_price[row['System']] = price
+
+    # Cost estimation
+    # TODO move outside of the function
+    def estimate_cost(row, system_to_price):
+        system = row['System']
+        price = system_to_price.get(system)
+        if price is None:
+            return None
+
+        hardware_quantity = row['Hardware quantity']
+        training_time = row['Training time (hours)']
+        hardware_model = row['Training hardware']
+        hardware_lifetime = DEFAULT_HARDWARE_LIFETIME
+
+        if any([np.isnan(x) for x in [hardware_quantity, training_time]]):
+            # Impute training time
+            # TODO: move this to a separate function
+            if not any([pd.isna(row[x]) for x in ['Training compute (FLOP)', 'Training hardware']]):
+                print("Imputing training time from compute and hardware")
+                flop = row['Training compute (FLOP)']
+                flop_per_second = get_flop_per_second(hardware_model, hardware_df)
+                flop_utilization = row['Hardware utilization']
+                if pd.isna(flop_utilization):
+                    flop_utilization = MEDIAN_UTILIZATION
+
+                training_chip_seconds = flop / (flop_per_second * flop_utilization)
+                training_chip_hours = training_chip_seconds / SECONDS_PER_HOUR
+            else:
+                print("Unable to estimate training chip hours")
+                return None
+        else:
+            training_chip_hours = training_time * hardware_quantity
+
+        total_chip_hours = training_chip_hours * 2  # TODO principled way to estimate total time
+
+        cost = 0
+
+        price_per_chip_hour = price / hardware_lifetime
+        amortized_hardware_cost = price_per_chip_hour * training_chip_hours
+        cost += amortized_hardware_cost
+
+        other_hardware_cost = cluster_interconnect_cost_per_gpu(hardware_model)
+        other_hardware_cost *= total_chip_hours / hardware_lifetime  # amortize
+        cost += other_hardware_cost
+
+        energy_cost = cluster_energy_cost(hardware_model, total_chip_hours, hardware_df)
+        cost += energy_cost
+
+        # Check for base model
+        if not pd.isna(row['Base model']):
+            base_model_name = row['Base model']
+            base_model = frontier_pcd_df[frontier_pcd_df['System'] == base_model_name].squeeze()
+            if base_model.empty:
+                print("Base model specified, but not found in database")
+                return None
+            base_cost = estimate_cost(base_model, system_to_price)
+            if base_cost is None:
+                print("Base model found, but unable to estimate cost")
+                return None
+            else:
+                cost += base_cost
+
+        return cost
+        
+    system_to_cost = {}
+    for i, row in frontier_pcd_df.iterrows():
+        print(f"==== System: {row['System']} ====")
+        cost = estimate_cost(row, system_to_price)
+        if cost is None:
+            print("Unable to estimate cost")
+            continue
+        else:
+            print("Estimated cost:", cost)
+        system_to_cost[row['System']] = cost
+
+    print(system_to_cost)
+
+    frontier_pcd_df['Cost'] = frontier_pcd_df['System'].map(system_to_cost)
+
+    return frontier_pcd_df
+
+
+def cluster_interconnect_cost_per_gpu(hardware_model):
+    # TODO: move to parameters.py
+    cluster_interconnect_cost_per_gbps = 11
+    # TODO: look up in hardware_df
+    cluster_interconnect_bandwidth = 300
+    cost_per_gpu = cluster_interconnect_cost_per_gbps * cluster_interconnect_bandwidth
+    return cost_per_gpu
+
+
+def cluster_energy_cost(hardware_model, total_chip_hours, hardware_df):
+    matching_hardware = hardware_df[hardware_df['Name of the hardware'] == hardware_model]
+    # TODO: handle missing values - look up similar hardware
+    chip_power_kw = matching_hardware['TDP (W)'].squeeze() / 1000
+    chip_power_kw *= SERVER_TDP_RATIO  # adjust for average power draw
+    chip_power_kw *= SERVER_CHIP_POWER_RATIO  # adjust for server power draw
+    chip_power_kw *= DEFAULT_PUE  # adjust for power distribution and cooling
+    cluster_kwh = chip_power_kw * total_chip_hours
+    return cluster_kwh * DATA_CENTER_ENERGY_PRICE
--- parameters.py
+++ parameters.py
@@ -5,3 +5,51 @@ SECONDS_PER_YEAR = SECONDS_PER_HOUR * 24 * DAYS_PER_YEAR
 DEFAULT_HARDWARE_LIFETIME = 3 * HOURS_PER_YEAR  # see e.g. https://gwern.net/doc/ai/scaling/hardware/2021-jouppi.pdf amortization of 3 years
 HARDWARE_COST_OVERHEAD = 1.6
 MEDIAN_UTILIZATION = 0.375  # median of 33 known values
+
+"""
+https://www.semianalysis.com/p/ai-datacenter-energy-dilemma-race
+"A typical enterprise colocation PUE is around 1.5-1.6, while most hyperscale datacenters are below 
+1.4 PUE, with some purpose build facilities (such as Google's) claim to achieve PUEs of below 1.10. 
+Most AI Datacenter specs aim for lower than 1.3 PUE."
+
+"For example at 80% utilization rate and a PUE of 1.25, [...]"
+
+TODO: if hyperscaler, set 1.1, else 1.25
+"""
+DEFAULT_PUE = 1.25
+
+"""
+https://pages.cs.wisc.edu/~shivaram/cs744-readings/dc-computer-v3.pdf, p.133
+For a typical new multi-megawatt data center, the authors assume the server's average power draw is
+75% of peak power.
+Note this may vary for different types of servers, e.g. for GPUs.
+
+TODO: maybe this should be closer to 100%: see p.6 of https://docs.nvidia.com/nvidia-dgx-superpod-data-center-design-dgx-h100.pdf
+"""
+SERVER_TDP_RATIO = 0.75
+
+"""
+Examples (using max specs):
+H100:
+- DGX 8x GPU: 10.2kW (https://resources.nvidia.com/en-us-dgx-systems/ai-enterprise-dgx)
+- GPU: 0.7kW (https://www.nvidia.com/en-us/data-center/h100/)
+- Ratio of per-GPU power: 1.82
+A100:
+- DGX 8x GPU: 6.5kW (https://www.nvidia.com/content/dam/en-zz/Solutions/Data-Center/nvidia-dgx-a100-datasheet.pdf)
+- GPU: 0.4kW (https://www.nvidia.com/en-us/data-center/a100/)
+- Ratio of per-GPU power: 2.03
+V100:
+- DGX-1 4x GPU: 1.5kW (https://www.nvidia.com/content/dam/en-zz/Solutions/Data-Center/dgx-station/dgx-station-print-explorer-datasheet-letter-final-web.pdf)
+- DGX-2 16x GPU: 10kW (https://www.nvidia.com/content/dam/en-zz/Solutions/Data-Center/dgx-station/dgx-station-print-explorer-datasheet-letter-final-web.pdf)
+- Ratio of per-GPU power: 1.25 to 2.08
+- GPU: 0.3kW (https://www.nvidia.com/en-gb/data-center/tesla-v100/)
+
+Average of ratios above: 1.8
+"""
+SERVER_CHIP_POWER_RATIO = 1.8
+
+"""
+https://www.eia.gov/electricity/monthly/epm_table_grapher.php?t=epmt_5_6_a
+Average US industrial electricity price as of January 2023: 8.32 Cents per Kilowatthour
+"""
+DATA_CENTER_ENERGY_PRICE = 0.083
