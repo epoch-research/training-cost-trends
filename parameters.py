d07daadb104bddde9bfcd25d392c1e45427f5f6d
Ben Cottier <bcottier2@gmail.com>
2024-05-13 16:38:08 -0400

    Neglect hardware failures in replacements parameter

--- parameters.py
+++ parameters.py
@@ -6,4 +6,8 @@ CLUSTER_INTERCONNECT_COST_OVERHEAD = 1.15  # TODO switch-case on known values?
 MEDIAN_UTILIZATION = 0.375  # median of 33 known values
 MEDIAN_TRAINING_TIME_DAYS = 793.5 / 24 # for running top-10 models
 ML_GPU_PRICE_PERFORMANCE_OOMS_PER_YEAR = 0.14  # https://epochai.org/blog/trends-in-machine-learning-hardware
-HARDWARE_REPLACEMENT_PER_YEAR = 0.51  # [90% CI: 0.36, 0.79]  replacements/year
+
+# 0.14 OOMs/year of hardware progress => optimum of 0.14 * np.log(10) replacements/year
+# https://epochai.org/blog/trends-in-machine-learning-hardware
+# https://epochai.org/blog/the-longest-training-run#a-simple-framework-for-training-run-lengths
+HARDWARE_REPLACEMENT_PER_YEAR = 0.31
